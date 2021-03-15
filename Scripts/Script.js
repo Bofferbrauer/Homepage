de3f941e2f726284fcab5545aabf6da93f146e40
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2021-03-15 06:40:48 +0100

    click

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,17 +1,17 @@
 const modal = document.querySelector("#CVModal");
 const button = document.querySelector("#CVpopup");
-const toClose = document.querySelector(".close")[0];
+const toClose = document.querySelector(".close");
 
 button.onclick = function () {
     modal.style.display = "block";
 }
 
-// toClose.onclick = function () {
-//     modal.style.display = "none";
-// }
 
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+toClose.onclick = function () {
+    modal.style.display = "none";
 }
