51c2a3b02687b2077ecdc59cc4b8e1b2f27090de
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2022-10-12 01:11:20 +0200

    Navbar javascript & footer position

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -15,3 +15,15 @@ window.onclick = function (event) {
 toClose.onclick = function () {
     modal.style.display = "none";
 }
+
+const navBar = document.querySelector("#navbar");
+const hamburgerMenu = document.querySelector(".fa-solid")
+const closeMenu = document.querySelector(".closeMenu")
+
+hamburgerMenu.onclick = function () {
+    navBar.style.width = "250px"
+}
+
+closeMenu.onclick = function () {
+    navBar.style.width = "0px"
+}
